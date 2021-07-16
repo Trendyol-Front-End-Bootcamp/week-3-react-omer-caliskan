e94c3e58093533596e0d2ce159d8de5e0cdaca7b
Ömer Çalışkan <50463226+omerclskn@users.noreply.github.com>
2021-07-16 21:33:48 +0300

    css fix

--- src/components/character/CharacterCard.js
+++ src/components/character/CharacterCard.js
@@ -4,7 +4,7 @@ import '../../assets/css/characterCard.css';
 function CharacterCard({char}) {
     return (
         <div className="list-item">
-            <h2>{char.name}</h2>
+            <h3>{char.name}</h3>
             <img src={char.image} alt="Rick and Morty" />
             <div className="item-prop">
         <span><b>Species:</b> {char.species}</span>
